17516af813b7d9add0f7d3b13b6740c3985f5db3
dinhtu44 <63580797+dinhtu44@users.noreply.github.com>
2022-03-31 10:17:49 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,17 +11,17 @@ provider "docker" {
   host = "npipe:////.//pipe//docker_engine"
 }
 
-resource "docker_image" "terraform_docker_app" {
-  name = "terraform_docker_app:latest"
-  keep_locally = false
+#resource "docker_image" "terraform_docker_app" {
+#  name = "terraform_docker_app:latest"
+#  keep_locally = false
   
-}
+#}
 
-resource "docker_container" "terraform_docker_app" {
-  image = docker_image.terraform_docker_app.latest
-  name  = "TuVD11-webApp-using-terraform"
-  ports {
-    internal = 5000
-    external = 5000
-  }
-}
+#resource "docker_container" "terraform_docker_app" {
+#  image = docker_image.terraform_docker_app.latest
+#  name  = "TuVD11-webApp-using-terraform"
+#  ports {
+#    internal = 5000
+#    external = 5000
+ # }
+#}
