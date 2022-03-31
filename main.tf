4a288722742e714d396785a332648d5522410b18
dinhtu44 <63580797+dinhtu44@users.noreply.github.com>
2022-03-31 10:05:43 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,24 +2,23 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = ">= 2.13.0"
+      version = "~> 2.7"
     }
   }
 }
 provider "docker" {
+  version = "~> 2.7"
   host = "npipe:////.//pipe//docker_engine"
 }
 
-resource "docker_image" "terraform-docker-app" {
-  name = "terraform-docker-app"
-  build {
-    path = "."
-    tag  = ["terraform-docker-app:latest"]
-  }
+resource "docker_image" "terraform_docker_app" {
+  name = "terraform_docker_app:latest"
+  keep_locally = false
+  
 }
 
-resource "docker_container" "terraform-docker-app" {
-  image = docker_image.terraform-docker-app.latest
+resource "docker_container" "terraform_docker_app" {
+  image = docker_image.terraform_docker_app.latest
   name  = "TuVD11-webApp-using-terraform"
   ports {
     internal = 5000
